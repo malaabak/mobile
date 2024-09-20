c051f3abd9c1e45e8fd68e8987726fe2debf69ce
JadH <jad@tomorrow.services>
2024-09-20 09:29:47 +0300

    continue setting up

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,4 @@
 module.exports = {
   presets: ["module:@react-native/babel-preset"],
-  plugins: [
-    "nativewind/babel",
-    [
-      "module-resolver",
-      {
-        root: ["./"],
-        extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
-        alias: {
-          "@resources": "./resources/",
-          "@components": "./src/presentation/common/components/",
-          "@domain": "./src/domain/",
-          "@data": "./src/data/",
-          "@presentation": "./src/presentation/",
-        },
-      },
-    ],
-  ],
+  plugins: ["nativewind/babel"],
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@ const colors = require("./resources/colors/tailwind")
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./App.{js,jsx,ts,tsx}",
-    "./src/presentation/**/*.{js,jsx,ts,tsx}",
-  ],
-  safelist: ["bg-blue-200", "bg-green-100", "text-blue-700", "border-blue-700"],
+  content: ["./App.{js,jsx,ts,tsx}", "./src/presentation/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors,
